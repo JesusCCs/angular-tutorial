ab043733ddaa97f86069d1e9f81bcb333560dcd6
JesusCCs <jesus.c.cobos@gmail.com>
2021-03-04 13:52:16 +0100

    Parte 1 del tutorial completa. Base para hacer la tarea 1

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,21 +1,25 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-import { products } from '../products';
+import { products } from "../products";
 
 @Component({
-  selector: 'app-product-list',
-  templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  selector: "app-product-list",
+  templateUrl: "./product-list.component.html",
+  styleUrls: ["./product-list.component.css"]
 })
-
-export class ProductListComponent {
+class ProductListComponent {
   products = products;
 
-  share() {
-    window.alert('The product has been shared!');
+  public share() {
+    window.alert("The product has been shared!");
+  }
+
+  onNotify() {
+    window.alert("You will be notified when the product goes on sale");
   }
 }
 
+export { ProductListComponent };
 
 /*
 Copyright Google LLC. All Rights Reserved.
