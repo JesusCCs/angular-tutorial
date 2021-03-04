d6debf3ef411a55030d1aa80010b1b8e3b92c2d6
JesusCCs <jesus.c.cobos@gmail.com>
2021-03-04 14:44:19 +0100

    Navegación

---
+++ src/app/product-details/product-details.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+
+import { products } from "../products";
+
+@Component({
+  selector: "app-product-details",
+  templateUrl: "./product-details.component.html",
+  styleUrls: ["./product-details.component.css"]
+})
+export class ProductDetailsComponent implements OnInit {
+  product;
+
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    const parametros = this.route.snapshot.paramMap;
+    const id = Number(parametros.get("id"));
+
+    this.product = products.find(product => product.id === id);
+  }
+}
