9d55e353f25573c0e01c8df999e499f1ee6325a3
JesusCCs <jesus.c.cobos@gmail.com>
2021-03-05 13:09:03 +0100

    Carrito

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 
 import { products } from "../products";
+import { CartService } from "../cart.service";
 
 @Component({
   selector: "app-product-details",
@@ -11,12 +12,21 @@ import { products } from "../products";
 export class ProductDetailsComponent implements OnInit {
   product;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    // Inyección de dependencias
+    private route: ActivatedRoute,
+    private cartService: CartService
+  ) {}
 
   ngOnInit() {
     const parametros = this.route.snapshot.paramMap;
     const id = Number(parametros.get("id"));
 
     this.product = products.find(product => product.id === id);
+  }
+
+  addToCart(product) {
+    this.cartService.addToCart(product);
+    window.alert("Se ha añadido tu producto al carrito");
   }
 }
